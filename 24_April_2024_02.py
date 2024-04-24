01c5c9dfedca566187684acab92c60886a231cff
Darshan Suresh <darshansuresh1804@gmail.com>
2024-04-24 19:35:10 +0530

    Create 24_April_2024_02.py

---
+++ 24_April_2024_02.py
@@ -0,0 +1,44 @@
+"""
+1137. N-th Tribonacci Number
+
+The Tribonacci sequence Tn is defined as follows: 
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+Given n, return the value of Tn.
+
+Example 1:
+
+Input: n = 4
+Output: 4
+Explanation:
+T_3 = 0 + 1 + 1 = 2
+T_4 = 1 + 1 + 2 = 4
+
+Example 2:
+
+Input: n = 25
+Output: 1389537
+
+Constraints:
+0 <= n <= 37
+The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+"""
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+
+        prev_prev = 0
+        prev = 1
+        curr = 1
+
+        for _ in range(3, n + 1):
+            next_val = prev_prev + prev + curr
+            prev_prev = prev
+            prev = curr
+            curr = next_val
+
+        return curr
